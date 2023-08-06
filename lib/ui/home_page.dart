d726042322e84c0e9c129dd73d4569edcbc0fded
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-06 23:57:44 +0300

    Handling details state

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:film_explorer/ui/titles_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../bloc/title_bloc.dart';
 import '../bloc/title_event.dart';
 import '../bloc/title_state.dart';
 import 'loading_page.dart';
+import 'title_details_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,8 +26,13 @@ class _HomePageState extends State<HomePage> {
         if (state is TitleSuccessFetchDataState) {
           return const TitlesScreen();
         }
+        if (state is TitleDetailsSuccessFetchState) {
+          return TitleDetailsScreen(
+            title: state.title,
+          );
+        }
         if (state is TitleErrorFetchDataState) {
-          return Text(state.errorMessage);
+          return Text(state.errorMessage); //TODO adequate error display
         }
         return const LoadingScreen();
       },
