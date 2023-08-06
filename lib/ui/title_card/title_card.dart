6174c5b0cf09fadff9bea766a87af940e209083d
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-06 23:55:13 +0300

    Card tap

--- lib/ui/title_card/title_card.dart
+++ lib/ui/title_card/title_card.dart
@@ -4,25 +4,29 @@ import '../../models/title.dart' as film_model;
 import 'card_poster_image.dart';
 
 class TitleCard extends StatelessWidget {
-  const TitleCard({required this.title, super.key});
+  const TitleCard({required this.title, required this.onTap, super.key});
 
   final film_model.Title title;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey[100],
-      child: Column(
-        children: [
-          CardPoster(
-            imageUrl: title.primaryImageURL,
-          ),
-          Text(
-            title.titleText,
-            textAlign: TextAlign.center,
-          ),
-          Text('${title.releaseYear}'),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        color: Colors.grey[100],
+        child: Column(
+          children: [
+            CardPoster(
+              imageUrl: title.primaryImageURL,
+            ),
+            Text(
+              title.titleText,
+              textAlign: TextAlign.center,
+            ),
+            Text('${title.releaseYear}'),
+          ],
+        ),
       ),
     );
   }
