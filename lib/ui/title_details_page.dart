0f40fa511a62bb52b56909532a874073fda8c222
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-07 20:15:34 +0300

    Details page update
    Working cast members output;
    Almost working writers/directors output

--- lib/ui/title_details_page.dart
+++ lib/ui/title_details_page.dart
@@ -1,10 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:film_explorer/bloc/title_bloc.dart';
 import 'package:film_explorer/bloc/title_event.dart';
+import 'package:film_explorer/data/constants/links.dart';
+import 'package:film_explorer/ui/divided_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:film_explorer/models/title.dart' as title_model;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_share/flutter_share.dart';
 
+import '../bloc/title_bloc.dart';
 import 'rating_display.dart';
 
 class TitleDetailsScreen extends StatefulWidget {
@@ -16,71 +19,116 @@ class TitleDetailsScreen extends StatefulWidget {
 }
 
 class _TitleDetailsScreenState extends State<TitleDetailsScreen> {
-  late final ScrollController _scrollController;
-  double _appBarOpacity = 1.0;
-
-  @override
-  void initState() {
-    _scrollController = ScrollController();
-    super.initState();
-    _scrollController.addListener(_handleScroll);
-  }
-
-  void _handleScroll() {
-    setState(() {
-      _appBarOpacity = _calculateAppBarOpacity();
-    });
-  }
-
-  double _calculateAppBarOpacity() {
-    double offset = _scrollController.offset;
-    double maxOffset = 100.0;
-    return 1.0 - (offset / maxOffset);
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     final title = widget.title;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.withOpacity(_appBarOpacity),
-        title: Text(title.titleText),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            context.read<TitleBloc>().add(FetchTitlesEvent(pageNumber: widget.title.pageNumber));
-          },
-        ),
-      ),
-      body: CustomScrollView(
-        controller: _scrollController,
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: CachedNetworkImage(
-                    imageUrl: title.primaryImageURL!,
-                    fit: BoxFit.contain,
-                  ),
+    return WillPopScope(
+      onWillPop: () async {
+        context.read<TitleBloc>().add(FetchTitlesEvent(
+              pageNumber: title.pageNumber,
+            ));
+        return false;
+      },
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    FlutterShare.share(
+                      title: title.titleText,
+                      linkUrl: '$imdbTitle/${title.id}',
+                    );
+                  },
+                  icon: const Icon(Icons.share),
                 ),
-                RatingDisplay(
-                    rating: (title.ratingsSummary['aggregateRating'] ?? 0)
-                        .toDouble()),
-                Text('Votes: ${title.ratingsSummary['voteCount']}'),
               ],
+              expandedHeight: 500,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('${title.titleText} (${title.releaseYear})'),
+                background: CachedNetworkImage(
+                  imageUrl: title.primaryImageURL!.isNotEmpty
+                      ? title.primaryImageURL!
+                      : noImageLink,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
             ),
-          ),
-          // Add other slivers for content here
-        ],
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Wrap(
+                        spacing: 8.0,
+                        children: (title.genres['genres'] as List<dynamic>)
+                            .map((genre) => Chip(label: Text(genre['text'])))
+                            .toList(),
+                      ),
+                    ),
+                    RatingDisplay(
+                        rating: (title.ratingsSummary['aggregateRating'] ?? 0)
+                            .toDouble()),
+                    if (title.ratingsSummary['aggregateRating'] != null)
+                      Text(title.ratingsSummary['aggregateRating'].toString()),
+                    Text('Votes: ${title.ratingsSummary['voteCount']}'),
+                    if (title.runtime != null)
+                      Text(title.runtime['displayableProperty']['value']
+                          ['plainText']),
+                    Text(title.plot['plotText']['plainText']),
+                    if (title.directors.isNotEmpty)
+                      DividedTab(
+                        alignment: Alignment.centerLeft,
+                        divider: const Divider(color: Colors.black),
+                        child: Text(
+                          'Directors: ${title.directors.map((item) => item.name).join(', ')}',
+                        ),
+                      ),
+                    if (title.writers.isNotEmpty)
+                      DividedTab(
+                        alignment: Alignment.centerLeft,
+                        divider: const Divider(color: Colors.black),
+                        child: Text(
+                            'Writers: ${title.writers.map((item) => item.name).join(', ')}'),
+                      ),
+                    if (title.cast.isNotEmpty)
+                      DividedTab(
+                        divider: const Divider(
+                          color: Colors.black,
+                        ),
+                        isLastTab: true,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Cast:',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: title.cast.length,
+                              itemBuilder: (context, index) {
+                                final castMember = title.cast[index];
+                                return Text(castMember.name);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
