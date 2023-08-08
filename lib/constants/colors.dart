7ecb5c7022abdb0481e4f04f397ca688609fedcd
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-08 21:51:46 +0300

    App colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color textColor = Colors.white;
+const Color backgroundColor = Color.fromARGB(255, 4, 9, 35);
+const Color primaryColor = Color.fromARGB(255, 44, 70, 186);
+const Color secondaryColor = Color.fromARGB(255, 34, 45, 94);
+const Color accentColor = Color.fromARGB(255,25, 64, 235);
