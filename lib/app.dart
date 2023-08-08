269b55273c1a416ce61633306818323c1499a4bb
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-08-08 21:54:24 +0300

    App theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,9 @@
-import 'package:film_explorer/ui/home_page.dart';
+import 'package:film_explorer/constants/colors.dart';
+import 'package:film_explorer/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'bloc/title_bloc.dart';
+import 'bloc/title/title_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        primaryColor: primaryColor,
+        colorScheme: const ColorScheme.light(
+          primary: primaryColor,
+          secondary: secondaryColor,
+        ),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+        ),
+      ),
       title: 'Film explorer',
       home: BlocProvider(
         create: (context) => TitleBloc(),
